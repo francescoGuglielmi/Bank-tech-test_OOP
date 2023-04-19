894e9b06a558763860d09fd92af76697cf5dcd19
Francesco Guglielmi <francescoguglielmi@Francescos-MacBook-Air.local>
2023-04-19 19:59:55 +0100

    test driven generate method of Statement class and fixed output of the other 2 methods

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -16,14 +16,12 @@ describe('statement', () => {
 
     test('it leaves an empty space if the transaction is not a credit', () => {
       const transaction = {type: 'debit', amount: 200, date: '19/04/2023', balance: 300};
-      
       expect(statement.formatCredit(transaction)).toBe(' ');
     })
 
     test('it returns the credit amount if the transaction is a credit', () => {
       const transaction = {type: 'credit', amount: 200, date: '19/04/2023', balance: 300};
-      
-      expect(statement.formatCredit(transaction)).toBe(200);
+      expect(statement.formatCredit(transaction)).toBe(' 200.00 ');
     })
 
   })
@@ -32,14 +30,26 @@ describe('statement', () => {
 
     test('it leaves an empty space if the transaction is not a credit', () => {
       const transaction = {type: 'credit', amount: 200, date: '19/04/2023', balance: 300};
-
       expect(statement.formatDebit(transaction)).toBe(' ')
     })
 
     test('it returns the debit amount if the transaction is a credit', () => {
       const transaction = {type: 'debit', amount: 200, date: '19/04/2023', balance: 300};
-      
-      expect(statement.formatDebit(transaction)).toBe(200);
+      expect(statement.formatDebit(transaction)).toBe(' 200.00 ');
+    })
+
+  })
+
+  describe('generate', () => {
+
+    test('it prints the statement for the account passed as a parameter of the class', () => {
+      const mockTransactions = [
+        {type: 'credit', amount: 500.00, date: '14/04/2023', balance: 500.00},
+        {type: 'debit', amount: 150.00, date: '15/04/2023', balance: 350.00},
+      ];
+      mockAccount.transactions = mockTransactions;
+
+      expect(statement.generate()).toBe('date || credit || debit || balance\n15/04/2023 || || 150.00 || 350.00\n14/04/2023 || 500.00 || || 500.00\n');
     })
 
   })
--- src/statement.js
+++ src/statement.js
@@ -5,15 +5,23 @@ class Statement {
   }
 
   formatCredit(transaction) {
-    return transaction.type === 'credit' ? transaction.amount : ' ';
+    return transaction.type === 'credit' ? (' ' + transaction.amount.toFixed(2) + ' ') : ' ';
   }
 
   formatDebit(transaction) {
-    return transaction.type === 'debit' ? transaction.amount : ' ';
+    return transaction.type === 'debit' ? (' ' + transaction.amount.toFixed(2) + ' ') : ' ';
   }
 
   generate() {
-    
+    let grid = 'date || credit || debit || balance\n'
+    const sortedTransactions = this.account.transactions.reverse();
+    for (let i = 0; i < sortedTransactions.length; i++) {
+      grid += sortedTransactions[i].date + ' ||' +
+              this.formatCredit(sortedTransactions[i]) + '||' +
+              this.formatDebit(sortedTransactions[i]) + '|| ' +
+              sortedTransactions[i].balance.toFixed(2) + '\n'
+    }
+    return grid
   }
 
 }
